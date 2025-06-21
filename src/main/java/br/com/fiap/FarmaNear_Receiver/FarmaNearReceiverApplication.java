89021c663d2f85ea5927db318da8d6b678db2be1
Ana Genova <anagenova9@outlook.com>
2025-06-21 17:37:17 -0300

    Creating project

---
+++ src/main/java/br/com/fiap/FarmaNear_Receiver/FarmaNearReceiverApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FarmaNear_Receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaNearReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaNearReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/FarmaNear_Receiver/FarmaNearReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.FarmaNear_Receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaNearReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
